01e7f9c1ce4373c18e6fc9746eb129200e00eb50
JafirRehman <mjafirrehman@gmail.com>
2024-05-17 11:50:12 +0500

    update newsletter

--- Client/src/Constants/Newsletter.jsx
+++ Client/src/Constants/Newsletter.jsx
@@ -18,23 +18,6 @@ const Newsletter = () => {
                     <input type="text" placeholder="Email Address" />
                     <button>Subscribe</button>
                 </div>
-                <span className="text">
-                    Will be used in accordance with our Privacy Policy
-                </span>
-                <span className="social-icons">
-                    <div className="icon">
-                        <FaLinkedinIn size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaFacebookF size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaTwitter size={14} />
-                    </div>
-                    <div className="icon">
-                        <FaInstagram size={14} />
-                    </div>
-                </span>
             </div>
         </div>
     );
