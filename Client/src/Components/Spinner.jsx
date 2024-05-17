dd040519f72c4e8f0815c75fc401dced9abb1919
JafirRehman <mjafirrehman@gmail.com>
2024-05-18 01:29:58 +0500

    product page success

--- Client/src/Components/Spinner.jsx
+++ Client/src/Components/Spinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Spinner = () => {
+const Spinner = ({status}) => {
     return (
-        <div className="flex justify-center items-center min-h-screen">
-            <div className="animate-spin h-10 w-10 border-t-4 border-blue-500 rounded-full"></div>
+        <div className={`flex justify-center items-center ${!status && 'min-h-screen'}`} >
+            <div className={`animate-spin h-8 w-8 border-t-4 border-blue-500 rounded-full`}></div>
         </div>
     )
 }
--- Client/src/Pages/ProductDetails.jsx
+++ Client/src/Pages/ProductDetails.jsx
@@ -2,11 +2,37 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Spinner from '../Components/Spinner';
+import { useDispatch } from 'react-redux';
+import { addtocartThunk } from '../redux/Slices/UserSlice';
+import { removefromcartThunk } from '../redux/Slices/UserSlice';
 
 const ProductDetails = () => {
   const { productid } = useParams();
+  const dispatch = useDispatch();
+
+  const userState = useSelector((state) => state.user);
+  const usercart = userState.user.cart;
+
   const products = useSelector(state => state.products.products)
   const product = products.find(product => product._id === productid)
+
+  const addToCart = (productId) => {
+    if (userState.user) {
+      dispatch(addtocartThunk(productId));
+    } else {
+      toast.error("Please login to add items to cart");
+      navigate("/login");
+    }
+  }
+  const removefromCart = (productId) => {
+    if (userState.user) {
+      dispatch(removefromcartThunk(productId));
+    } else {
+      toast.error("Please login to remove items from cart");
+      navigate("/login");
+    }
+  }
+
   return (
     <div className="mx-auto p-4 h-screen max-w-[70%]">
       {
@@ -21,13 +47,25 @@ const ProductDetails = () => {
               <p className="mt-4 text-zinc-600 capitalize">{`Brand : ${product.category}`}</p>
               <p className="mt-4 text-zinc-600">{`Rs . ${product.price}`}</p>
               <div className="mt-4">
-                <button className="bg-[#0E0E11] text-ourred-50 hover:scale-90 transition-all duration-200 px-4 py-2 rounded-lg mt-4">ADD TO CART</button>
+                {
+                  usercart?.length > 0 && usercart?.some((pro) => pro._id === product._id) ?
+                    (<button
+                      className="bg-[#0E0E11] text-ourred-50 hover:scale-90 transition-all duration-200 h-10 w-40 rounded-lg mt-4"
+                      onClick={() => { removefromCart(product._id) }}>
+                      {userState && userState.isLoading ? <Spinner status={true} /> : "Remove Item"}
+                    </button>) :
+                    (<button
+                      className="bg-[#0E0E11] text-ourred-50 hover:scale-90 transition-all duration-200 h-10 w-40 rounded-lg mt-4"
+                      onClick={() => { addToCart(product._id) }}>
+                      {userState && userState.isLoading ? <Spinner status={true} /> : "Add to Cart"}
+                    </button>)
+                }
               </div>
             </div>
           </div>
         )
       }
-      {!product && <Spinner/>}
+      {!product && <Spinner />}
     </div>
   )
 }
@@ -39,18 +77,3 @@ export default ProductDetails
               <span>1</span>
               <button className="bg-zinc-200 text-black px-2 py-1 rounded">+</button>
             </div> */
-
-/**<div className="flex flex-col md:flex-row h-[70%] mt-10 w-full">
-<div className="flex w-[50%] justify-center items-center bg-[#F2F2F2]">
-<img src="https://res.cloudinary.com/dxrnq0wcs/image/upload/v1715772539/Products/vqb5p8o9s6ayzjd43fvo.png" alt="product" className=""/>
-</div>
-<div className="w-[50%] flex flex-col justify-between ml-0 md:ml-8 mt-4 md:mt-0">
-<h2 className="text-xl font-bold">hello</h2>
-<p className="text-lg text-zinc-800 mt-2">hello</p>
-<p className="mt-4 text-zinc-600">hello</p>
-<p className="mt-4 text-zinc-600">hello</p>
-<div className="mt-4">
-<button className="bg-[#0E0E11] text-ourred-50 hover:scale-90 transition-all duration-200 px-4 py-2 rounded-lg mt-4">ADD TO CART</button>
-</div>
-</div>
-</div> */
