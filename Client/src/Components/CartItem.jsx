af1971dacd36279a42579ca076fd448a026f249b
JafirRehman <mjafirrehman@gmail.com>
2024-05-18 23:23:46 +0500

    update cartitem

--- Client/src/Components/CartItem.jsx
+++ Client/src/Components/CartItem.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-hot-toast';
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, isLoading }) => {
 
     const userState = useSelector((state) => state.user);
 
@@ -24,21 +24,18 @@ const CartItem = ({ item }) => {
     return (
 
         <div className="p-4 border-b-2 last:border-none border-slate-700">
-            <div className="flex justify-between py-3.5 px-2.5 gap-14 flex-col md:flex-row">
+            <div className="flex justify-between items-center py-3.5 px-2.5 gap-14 flex-col md:flex-row">
                 <div className="md:w-[30%] w-full flex justify-center items-center">
                     <img src={item.image} alt="" className="w-[40%] max-h-[200px] md:w-[50%] lg:w-full" />
                 </div>
                 <div className="md:w-[70%] w-full flex flex-col gap-5">
                     <h1 className="text-xl font-[600] text-slate-700">{item.title}</h1>
-                    <h1 className="text-slate-700">{
-                        item.description.split(" ").slice(0, 15).join(" ") + "..."}
-                    </h1>
                     <div className="flex justify-between">
-                        <p className="font-bold text-[#16a34a] text-lg">${item.price}</p>
+                        <p className="font-bold text-[#16a34a] text-lg">Rs. <span className="text-[#0E0E11]">{item.price}</span></p>
                         <div
                             onClick={() => { removefromCart(item._id) }}
-                            className="w-10 h-10 rounded-full bg-red-200 flex justify-center items-center hover:bg-red-400 group transition-all">
-                            <MdDeleteSweep className="text-ourred-800 transition-all" />
+                            className="p-1 rounded-full bg-ourred-700 flex justify-center items-center hover:bg-red-400 group transition-all">
+                            <MdDeleteSweep className="text-ourred-50 text-[20px]"/>
                         </div>
                     </div>
                 </div>
--- Client/src/Pages/Cartpage.jsx
+++ Client/src/Pages/Cartpage.jsx
@@ -12,9 +12,9 @@ const Cartpage = () => {
     setTotalAmount(user?.cart?.reduce((previous, item) => previous + parseInt(item.price), 0))
   }, [userState])
 
-  const navigate=useNavigate()
+  const navigate = useNavigate()
   useEffect(() => {
-    if(!userState?.user) {
+    if (!userState?.user) {
       navigate('/login')
     }
   })
@@ -26,25 +26,22 @@ const Cartpage = () => {
             <div className="lg:w-[70%]">
               {
                 user.cart.map((item, index) => {
-                  return <CartItem key={item._id} item={item} itemIndex={index} />
+                  return <CartItem key={item._id} item={item} isLoading={userState.isLoading} itemIndex={index} />
                 })
               }
             </div>
             <div className="md:w-[30%] w-full flex flex-col gap-8 justify-between">
 
               <div className="mt-20">
-                <p className="text-xl text-[#166534] uppercase font-[600]">Your Cart</p>
-                <p className="text-5xl font-[600] text-[#15803d] uppercase mb-4">Summary</p>
+                <p className="text-xl text-[#0E0E11] uppercase font-[600]">Your Cart</p>
+                <p className="text-5xl font-[600] text-[#0E0E11] uppercase mb-4">Summary</p>
                 <p className="font-[600] text-xl text-slate-700">
                   Total Items: <span className="font-normal">{user.cart.length}</span>
                 </p>
-              </div>
-
-              <div className="mb-20">
-                <p className="text-slate-700 text-xl font-[600] mb-5 ">Total Amount:
-                  <span className="font-bold ml-2 text-black">${totalAmount}</span>
+                <p className="mt-5 text-slate-700 text-xl font-[600] mb-5 ">Total Amount:
+                  <span className="font-bold ml-2 text-[#16a34a]">Rs. <span className="text-[#0E0E11]">{totalAmount}</span></span>
                 </p>
-                <button onClick={()=> navigate('/user/cart/order')} className="bg-[#16a34a] text-white text-md uppercase font-[600] py-3 px-10 rounded-md border-[#16a34a] border-2 hover:bg-white hover:scale-75 transition-all duration-300">
+                <button onClick={() => navigate('/user/cart/order')} className="bg-[#0E0E11] text-ourred-50 text-md uppercase font-[600px] py-3 px-10 rounded-md hover:scale-90 transition-all duration-300">
                   CheckOut Now
                 </button>
               </div>
