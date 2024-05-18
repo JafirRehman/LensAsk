5cbb0f3df0ce5302c1414e0417cce71bfcc460c5
JafirRehman <mjafirrehman@gmail.com>
2024-05-18 20:35:43 +0500

    update

--- Server/handlers/Common_handlers.js
+++ Server/handlers/Common_handlers.js
@@ -1,44 +1,10 @@
 
 const Product = require('../models/Product')
 
-//GetWomenProducts
-exports.GetWomenProducts = async (req, res) => {
-    try {
-        const allWomenProducts = await Product.find({category:'women'});
-        //return response
-        return res.status(200).json({
-            success: true,
-            data: allWomenProducts,
-        });
-    } catch (error) {
-        return res.status(404).json({
-            success: false,
-            message: "something went wrong in GetWomenProducts try block",
-        });
-    }
-};
-//GetMenProducts
-exports.GetMenProducts = async (req, res) => {
-    try {
-        const allMenProducts = await Product.find({category:'men'});
-        //return response
-        return res.status(200).json({
-            success: true,
-            data: allMenProducts,
-        });
-    } catch (error) {
-        return res.status(404).json({
-            success: false,
-            message: "something went wrong in GetMenProducts try block",
-        });
-    }
-};
-
-
 //getAllProducts
 exports.GetAllProducts = async (req, res) => {
     try {
-        const allProducts = await Product.find({});
+        const allProducts = await Product.find({}).sort({ createdAt: -1 });
         //return response
         return res.status(200).json({
             success: true,
--- Server/routes/Common_routes.js
+++ Server/routes/Common_routes.js
@@ -1,12 +1,11 @@
 const express=require('express')
 
 const {GetAllProducts}=require('../handlers/Common_handlers')
-//const {GetMenProducts,GetWomenProducts}=require('../handlers/Common_handlers')
+
 
 const router = express.Router()
 
 router.get("/getallproducts", GetAllProducts)
-//router.get("/getmenproducts", GetMenProducts)
-//router.get("/getwomenproducts", GetWomenProducts)
+
 
 module.exports = router
