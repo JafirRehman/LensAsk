9254f67d9380765570dcea9d005881bc442a0210
JafirRehman <mjafirrehman@gmail.com>
2024-05-26 17:09:28 +0500

    necessary updates

--- Client/src/Pages/Customer/CreateOrderpage.jsx
+++ Client/src/Pages/Customer/CreateOrderpage.jsx
@@ -3,15 +3,42 @@ import { useState } from 'react'
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import Spinner from '../../Components/Spinner';
-import { useSelector } from 'react-redux';
 import { updateuser } from '../../redux/Slices/UserSlice'
 import { useDispatch } from 'react-redux';
-
+import { useEffect } from 'react';
 const CreateOrderpage = () => {
-
+    const [loading, setLoading] = useState(true);
     const [isloading, setIsloading] = useState(false)
 
-    let userState = useSelector(state => state.user)
+    async function fetchProductData() {
+        setLoading(true);
+        try {
+            const res = await fetch("http://localhost:5000/AuthCommon/userdetails", {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                dispatch(updateuser(data.existeduser))
+            } else {
+                const data = await res.json();
+                toast.error(data.message);
+            }
+        }
+        catch (error) {
+            toast.error("something went wrong");
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchProductData()
+    }, [])
+
 
     const [formdata, setFormdata] = useState({
         receiverName: '',
@@ -59,27 +86,36 @@ const CreateOrderpage = () => {
     }
 
     return (
-        <div className='h-screen w-[90%]  max-w-[1250px] mx-auto flex justify-center'>
-            <div className='bg-[#F1F2F3] self-start py-3 mt-3 px-20'>
-                <form onSubmit={formhandler} className=' flex flex-col text-[1.2rem] mobile:w-96 self-start mobile:text-[1.2rem]'>
-                    <label htmlFor='receiverName' className='mt-2 mb-2'>Name<span className='text-ourred-500'>*</span></label>
-                    <input onChange={changeformvalues} name='receiverName' value={formdata.receiverName} required id='receiverName' className='text-[1rem] h-[50px] p-5 focus:outline-none text-white' type='text' placeholder='Enter Receiver Name'></input>
-                    <label htmlFor='email' className='mt-2 mb-2'>Email<span className='text-ourred-500'>*</span></label>
-                    <input onChange={changeformvalues} name='email' value={formdata.email} required id='email' className='  text-[1rem] h-[50px] p-5 focus:outline-none text-white ' type='email' placeholder='Enter Email Address'></input>
-                    <label htmlFor='address' className='mt-2 mb-2'>Address<span className='text-ourred-500'>*</span></label>
-                    <input onChange={changeformvalues} name='address' value={formdata.address} required id='address' className=' text-[1rem] h-[50px] p-5 focus:outline-none text-white ' type='text' placeholder='Enter Address'></input>
-                    <label htmlFor='phoneNumber' className='mt-2 mb-2'>Phone Number<span className='text-ourred-500'>*</span></label>
-                    <input onChange={changeformvalues} name='phoneNumber' value={formdata.phoneNumber} required id='phoneNumber' className=' text-[1rem] h-[50px] p-5 focus:outline-none text-white' type='text' placeholder='ex - 03000000000'></input>
-                    <button type='submit' className=' flex items-center justify-center text-ourred-50 mt-10 bg-[#0E0E11] text-[1.2rem] sm:text-[1.5rem] pt-5 pb-5 max-h-9 min-h-9 hover:scale-[0.93] transition-transform duration-300'>
-                        {isloading ? (
-                            <Spinner />
-                        ) : (
-                            'Create Order'
-                        )}
-                    </button>
-                </form>
-            </div>
-        </div>
+        <>
+            {
+                loading && <Spinner />
+            }
+            {
+                !loading && (
+                    <div className='h-screen w-[90%]  max-w-[1250px] mx-auto flex justify-center'>
+                        <div className='bg-[#F1F2F3] self-start py-3 mt-3 px-20'>
+                            <form onSubmit={formhandler} className=' flex flex-col text-[1.2rem] mobile:w-96 self-start mobile:text-[1.2rem]'>
+                                <label htmlFor='receiverName' className='mt-2 mb-2'>Name<span className='text-ourred-500'>*</span></label>
+                                <input onChange={changeformvalues} name='receiverName' value={formdata.receiverName} required id='receiverName' className='text-[1rem] h-[50px] p-5 focus:outline-none text-white' type='text' placeholder='Enter Receiver Name'></input>
+                                <label htmlFor='email' className='mt-2 mb-2'>Email<span className='text-ourred-500'>*</span></label>
+                                <input onChange={changeformvalues} name='email' value={formdata.email} required id='email' className='  text-[1rem] h-[50px] p-5 focus:outline-none text-white ' type='email' placeholder='Enter Email Address'></input>
+                                <label htmlFor='address' className='mt-2 mb-2'>Address<span className='text-ourred-500'>*</span></label>
+                                <input onChange={changeformvalues} name='address' value={formdata.address} required id='address' className=' text-[1rem] h-[50px] p-5 focus:outline-none text-white ' type='text' placeholder='Enter Address'></input>
+                                <label htmlFor='phoneNumber' className='mt-2 mb-2'>Phone Number<span className='text-ourred-500'>*</span></label>
+                                <input onChange={changeformvalues} name='phoneNumber' value={formdata.phoneNumber} required id='phoneNumber' className=' text-[1rem] h-[50px] p-5 focus:outline-none text-white' type='text' placeholder='ex - 03000000000'></input>
+                                <button type='submit' className=' flex items-center justify-center text-ourred-50 mt-10 bg-[#0E0E11] text-[1.2rem] sm:text-[1.5rem] pt-5 pb-5 max-h-9 min-h-9 hover:scale-[0.93] transition-transform duration-300'>
+                                    {isloading ? (
+                                        <Spinner />
+                                    ) : (
+                                        'Create Order'
+                                    )}
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                )
+            }
+        </>
     )
 }
 
--- Client/src/Pages/Profilepage.jsx
+++ Client/src/Pages/Profilepage.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux';
 import Intro from '../Components/Profile/Intro'
+import Spinner from '../Components/Spinner';
+import toast from 'react-hot-toast';
+import { updateuser } from "../redux/Slices/UserSlice"
 
 const Profilepage = () => {
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch()
+    
+    async function fetchProductData() {
+        setLoading(true);
+        try {
+            const res = await fetch("http://localhost:5000/AuthCommon/userdetails", {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+                dispatch(updateuser(data.existeduser))
+            } else {
+                const data = await res.json();
+                toast.error(data.message);
+            }
+        }
+        catch (error) {
+            toast.error("something went wrong");
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchProductData()
+    }, [])
+
     const userState = useSelector(state => state.user)
+    
     return (
         <div className='w-full mt-10'>
             {
-                userState && (
-                    <Intro userState={userState}/>
+                !loading && userState && (
+                    <Intro userState={userState} />
+                )
+            }
+            {
+                loading && (
+                    <Spinner />
                 )
             }
         </div>
