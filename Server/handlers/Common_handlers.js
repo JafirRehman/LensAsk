5dd3d27d912ac6b839cd6f7dfdc29d5a5b1d320b
JafirRehman <mjafirrehman@gmail.com>
2024-09-05 22:16:43 -0700

    update

--- Server/handlers/Common_handlers.js
+++ Server/handlers/Common_handlers.js
@@ -1,48 +1,46 @@
-const Subscriber = require('../models/Subscriber');
-const Product = require('../models/Product')
-
+const Subscriber = require("../models/Subscriber");
+const Product = require("../models/Product");
 
 //subscribe
 exports.Subscriberroute = async (req, res) => {
-    const { email } = req.body;
-    try {
-
-        const findsubcriber = await Subscriber.findOne(email)
-        if (findsubcriber) {
-            return res.status(400).json({
-                success: false,
-                message: 'User Already Registered!',
-            });
-        }
-
-        await Subscriber.create({ email });
-
-        return res.status(200).json({
-            success: true,
-            message: 'Subscribed Successfully',
-        });
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            success: false,
-            message: 'Server error'
-        });
+  const { email } = req.body;
+  try {
+    const findsubcriber = await Subscriber.findOne({ email });
+    if (findsubcriber) {
+      return res.status(400).json({
+        success: false,
+        message: "User Already Registered!",
+      });
     }
+
+    await Subscriber.create({ email });
+
+    return res.status(200).json({
+      success: true,
+      message: "Subscribed Successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
 };
 
 //getAllProducts
 exports.GetAllProducts = async (req, res) => {
-    try {
-        const allProducts = await Product.find({}).sort({ createdAt: -1 });
-        //return response
-        return res.status(200).json({
-            success: true,
-            data: allProducts,
-        });
-    } catch (error) {
-        return res.status(404).json({
-            success: false,
-            message: "something went wrong in GetAllProducts try block",
-        });
-    }
+  try {
+    const allProducts = await Product.find({}).sort({ createdAt: -1 });
+    //return response
+    return res.status(200).json({
+      success: true,
+      data: allProducts,
+    });
+  } catch (error) {
+    return res.status(404).json({
+      success: false,
+      message: "something went wrong in GetAllProducts try block",
+    });
+  }
 };
--- Server/index.js
+++ Server/index.js
@@ -8,13 +8,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const Admin_routes = require("./routes/Admin_routes");
-const Common_routes= require('./routes/Common_routes')
-const Auth_routes= require('./routes/Auth_routes')
-const Customer_routes= require('./routes/Customer_routes')
-const AuthCommon_routes= require('./routes/AuthCommon_routes')
+const Common_routes = require("./routes/Common_routes");
+const Auth_routes = require("./routes/Auth_routes");
+const Customer_routes = require("./routes/Customer_routes");
+const AuthCommon_routes = require("./routes/AuthCommon_routes");
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 // Connecting to database and cloudinary
@@ -22,37 +21,36 @@ connectDB();
 cloudinaryConnect();
 
 // Middlewares
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: "/tmp/",
-	})
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
 );
 const corsOptions = {
-	origin: true,
-	optionsSuccessStatus: 200,
-    credentials:true,
-}
+  origin: true,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
-const { auth, isCustomer, isAdmin } = require("./middlewares/Auth_middlewares")
+const { auth, isCustomer, isAdmin } = require("./middlewares/Auth_middlewares");
 
 // Testing the server
 
-app.use("/admin", auth , isAdmin, Admin_routes );
-app.use("/common", Common_routes );
-app.use("/Auth", Auth_routes );
-app.use("/AuthCommon",auth, AuthCommon_routes);
-app.use("/customer", auth , isCustomer ,Customer_routes);
-
+app.use("/admin", auth, isAdmin, Admin_routes);
+app.use("/common", Common_routes);
+app.use("/Auth", Auth_routes);
+app.use("/AuthCommon", auth, AuthCommon_routes);
+app.use("/customer", auth, isCustomer, Customer_routes);
 
 // Setting up port number
 const PORT = process.env.PORT;
 
 // Listening to the server
 app.listen(PORT, () => {
-    console.log(`App is running at ${PORT}`);
+  console.log(`App is running at ${PORT}`);
 });
