ced6682dc86bc60233960836b07ae7c9e5112633
JafirRehman <mjafirrehman@gmail.com>
2024-10-20 18:01:59 +0500

    fetch all products

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,36 +1,55 @@
 import Banner from "../Constants/Banner";
 import "../styles/Home.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchProducts } from "../redux/Slices/ProductsSlice";
-import ProductsSlide from "../Components/ProductsSlide";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Spinner from "../Components/Spinner";
+import ProductsSlide from "../Components/ProductsSlide";
 
 const Home = () => {
-    const dispatch= useDispatch();
-    const productsState = useSelector((state) => state.products);
-    useEffect(() => {
-        dispatch(fetchProducts());
-    },[])
-    return (
-        <div className="">
-            <Banner />
-            {productsState.isLoading && <Spinner />}
-            {productsState && !productsState.isLoading && productsState.products.length > 0 && (
-                <ProductsSlide productsState={productsState}/>
-            )}
-            {productsState && !productsState.isLoading && productsState.products.length === 0 && (
-                <div className="w-screen h-screen flex justify-center items-center">
-                    <p className="font-bold">No Data Found</p>
-                </div>
-            )}
-            {productsState && !productsState.isLoading && productsState.error && (
-                <div className="w-screen h-screen flex justify-center items-center">
-                    <p className="font-bold">Error: Got error while getting data</p>
-                </div>
-            )}
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BACKEND_BASE_URL}/common/getallproducts`
+      );
+      if (!response.ok) {
+        setError("Network Issue");
+      } else {
+        const data = await response.json();
+        setProducts(data.data);
+      }
+    } catch (error) {
+      setError("Server Issue");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="">
+      <Banner />
+      {error ? (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <p className="font-bold">{error}</p>
         </div>
-    );
+      ) : isLoading ? (
+        <Spinner />
+      ) : products.length > 0 ? (
+        <ProductsSlide productsState={products} />
+      ) : (
+        <div className="w-screen h-screen flex justify-center items-center">
+          <p className="font-bold">No Data Found</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- Client/src/redux/Slices/ProductsSlice.js
+++ Client/src/redux/Slices/ProductsSlice.js
@@ -1,23 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-//const base_url = "https://fakestoreapi.com/";
+const base_url = import.meta.env.VITE_API_BACKEND_BASE_URL;
 
-const base_url = "http://localhost:5000/";
-
-export const fetchProducts = createAsyncThunk("fetch/products", async (_,{rejectWithValue}) => {
+export const fetchProducts = createAsyncThunk(
+  "fetch/products",
+  async (_, { rejectWithValue }) => {
     try {
-        //const response = await fetch(`${base_url}products`);
-        const response = await fetch(`${base_url}common/getallproducts`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        return data;
+      const response = await fetch(`${base_url}/common/getallproducts`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data;
     } catch (error) {
-        console.error('Error:', error)
-        return rejectWithValue(error);
+      console.error("Error:", error);
+      return rejectWithValue(error);
     }
-});
+  }
+);
 
 const initialState = {
   isLoading: false,
@@ -30,18 +30,17 @@ const productSlice = createSlice({
   initialState,
   extraReducers: (builder) => {
     builder
-      .addCase(fetchProducts.pending, (state, action) => {
+      .addCase(fetchProducts.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(fetchProducts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.products = action.payload.data;
-        
       })
       .addCase(fetchProducts.rejected, (state, action) => {
         state.isLoading = false;
         state.products = [];
-        state.error=action.payload
+        state.error = action.payload;
       });
   },
 });
