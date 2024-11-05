ec0a77cf6fabdba8aa71b627ef442065b7e56fae
JafirRehman <mjafirrehman@gmail.com>
2024-11-05 23:27:26 +0500

    fixing another bug

--- Client/src/Pages/Admin/Orderspage.jsx
+++ Client/src/Pages/Admin/Orderspage.jsx
@@ -13,7 +13,14 @@ const Orderspage = () => {
       setIsloading(true);
       try {
         const res = await fetch(
-          `${import.meta.env.VITE_API_BACKEND_BASE_URL}/admin/getallorders`
+          `${import.meta.env.VITE_API_BACKEND_BASE_URL}/admin/getallorders`,
+          {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
         );
         const data = await res.json();
         setOrders(data.orders);
@@ -30,7 +37,7 @@ const Orderspage = () => {
     <>
       {isloading ? (
         <Spinner />
-      ) : orders.length === 0 ? (
+      ) : orders?.length === 0 ? (
         <div className="h-screen flex items-center justify-center bg-zinc-100 dark:bg-zinc-900">
           <p className="bg-[#F1F2F3] p-5 text-2xl text-center">
             There Are No Orders!
@@ -38,7 +45,7 @@ const Orderspage = () => {
         </div>
       ) : (
         <div>
-          {orders.map((order) => {
+          {orders?.map((order) => {
             return <Ordercomponent key={order._id} order={order} />;
           })}
         </div>
