a62e41b8464f6d2cf2838a37713c7a6ec3949bd3
JafirRehman <mjafirrehman@gmail.com>
2024-11-10 23:48:22 +0500

    final touch to webhook

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-//connections for db and cloudinary
+
 const connectDB = require("./config/database");
 const { cloudinaryConnect } = require("./config/cloudinary");
-
-const User = require("./models/User");
-const Order = require("./models/Order");
 
 const OnlyAdmin_routes = require("./routes/OnlyAdmin");
 const Public_routes = require("./routes/Public");
 const Auth_routes = require("./routes/Auth");
 const OnlyCustomer_routes = require("./routes/OnlyCustomers");
 const User_routes = require("./routes/User");
+const WebHook_route = require("./routes/WebHook");
 
 const { auth, isCustomer, isAdmin } = require("./middlewares/Auth_middlewares");
 
@@ -39,60 +37,12 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-// Webhook
-app.post(
-  "/webhook",
-  express.raw({ type: "application/json" }),
-  async (req, res) => {
-    const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+// Use the webhook route with express.raw() middleware
+app.use("/webhook", webhookRoute);
 
-    const payload = req.body;
-    const sig = req.headers["stripe-signature"];
-
-    let event;
-    try {
-      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
-    } catch (err) {
-      res.status(400).send(`Webhook Error: ${err.message}`);
-      return;
-    }
-    if (event.type === "checkout.session.completed") {
-      const checkoutSession = event.data.object;
-      const customerAddress = checkoutSession.customer_details?.address;
-      const customerName = checkoutSession.customer_details?.name;
-      const customerEmail = checkoutSession.customer_details?.email;
-      const id = checkoutSession.client_reference_id;
-      const totalPrice = parseFloat(
-        checkoutSession.amount_total / 100
-      ).toString();
-      const addressString = `${customerAddress.city || ""} ${
-        customerAddress.line2 || ""
-      } ${customerAddress.line1 || ""} ${customerAddress.country || ""}
-      `.trim();
-
-      const user = await User.findById(id).populate("cart.product");
-
-      await Order.create({
-        userid: id,
-        receiverName: customerName,
-        email: customerEmail,
-        address: addressString,
-        totalPrice: totalPrice,
-        products: user.cart,
-      });
-
-      await User.findByIdAndUpdate(id, {
-        $set: { cart: [] },
-      });
-    }
-    res.send();
-  }
-);
-
+// Use JSON middleware for all other routes
 app.use(express.json());
-// Testing the server
 
 app.use("/admin", auth, isAdmin, OnlyAdmin_routes);
 app.use("/common", Public_routes);
@@ -100,7 +50,6 @@ app.use("/auth", Auth_routes);
 app.use("/authCommon", auth, User_routes);
 app.use("/customer", auth, isCustomer, OnlyCustomer_routes);
 
-// Setting up port number
 const PORT = process.env.PORT;
 
 // Listening to the server
---
+++ Server/routes/WebHook.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const User = require("../models/User");
+const Order = require("../models/Order");
+const mailsender = require("../utils/mailsender");
+
+const router = express.Router();
+
+// Webhook
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+router.post(
+  "/",
+  express.raw({ type: "application/json" }),
+  async (req, res) => {
+    const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+    const payload = req.body;
+    const sig = req.headers["stripe-signature"];
+
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+    } catch (err) {
+      res.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+    if (event.type === "checkout.session.completed") {
+      const checkoutSession = event.data.object;
+      const customerAddress = checkoutSession.customer_details?.address;
+      const customerName = checkoutSession.customer_details?.name;
+      const customerEmail = checkoutSession.customer_details?.email;
+      const id = checkoutSession.client_reference_id;
+      const totalPrice = parseFloat(
+        checkoutSession.amount_total / 100
+      ).toString();
+      const addressString = `${customerAddress.city || ""} ${
+        customerAddress.line2 || ""
+      } ${customerAddress.line1 || ""} ${customerAddress.country || ""}
+        `.trim();
+
+      const user = await User.findById(id).populate("cart.product");
+
+      const ourneworder = await Order.create({
+        userid: id,
+        receiverName: customerName,
+        email: customerEmail,
+        address: addressString,
+        totalPrice: totalPrice,
+        products: user.cart,
+      });
+
+      await User.findByIdAndUpdate(id, {
+        $set: { cart: [] },
+      });
+
+      try {
+        await mailsender(
+          ourneworder.email,
+          "Order Confirmation",
+          `<div style="max-width: 32rem; margin: 0 auto; padding: 1rem; background-color: #ffffff; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); border-radius: 0.5rem; color: #374151;">
+                    <h1 style="font-size: 1.25rem; font-weight: 700; color: #374151;">Order Confirmation</h1>
+                    <p style="font-size: 0.875rem; color: #6B7280; margin-top: 0.5rem;">Hi ${ourneworder.receiverName},</p>
+                    <p style="font-size: 0.875rem; color: #6B7280; margin-top: 0.5rem;">Thank you for your order. We are happy to confirm that your order has been received and is being processed.</p>
+                    <div style="margin-top: 1rem;">
+                        <p style="font-size: 0.875rem; color: #6B7280;">Order Details:</p>
+                        <ul style="font-size: 0.875rem; color: #6B7280; list-style-type: disc; list-style-position: inside; margin-top: 0.5rem;">
+                            <li>Order ID: ${ourneworder._id}</li>
+                            <li>Address: ${ourneworder.address}</li>
+                            <li>Total: Rs.${ourneworder.totalPrice}</li>
+                        </ul>
+                    </div>
+                    <p style="font-size: 0.875rem; color: #6B7280; margin-top: 1rem;">You will receive a shipping confirmation email once your order has been dispatched.</p>
+                    <p style="font-size: 0.875rem; color: #6B7280; margin-top: 1rem;">Thank you for shopping with us!</p>
+                    <p style="font-size: 0.875rem; color: #6B7280; margin-top: 0.5rem;">Best Regards,</p>
+                    <p style="font-size: 0.875rem; color: #6B7280;">LensAsk</p>
+                </div>`
+        );
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+    res.send();
+  }
+);
+
+module.exports = router;
