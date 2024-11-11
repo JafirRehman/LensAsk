bd15c5c22f25413da61a1efb7dd0b52d699d99aa
JafirRehman <mjafirrehman@gmail.com>
2024-11-12 02:22:49 +0500

    updating ui of home page 2

--- Client/src/Components/Home/Banner.jsx
+++ Client/src/Components/Home/Banner.jsx
@@ -4,7 +4,7 @@ import "../../styles/Banner.scss";
 const Banner = () => {
   const navigate = useNavigate();
   return (
-    <div className="hero-banner">
+    <div className="hero-banner py-[80px]">
       <div className="content">
         <div className="text-content">
           <h1>Style meets vision.</h1>
