c9d31d6de046821a395fbe736ff93d92d94f778c
JafirRehman <mjafirrehman@gmail.com>
2024-12-15 10:23:54 +0500

    fixing schema

--- Server/models/Product.js
+++ Server/models/Product.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // product schema
 const productSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Product Name is required'],
-        trim: true,
-        maxlength: [120, 'Product Name Must Be Less Than 120 characters!'],
-    },
-    price: {
-        type: String,
-        required: [true, 'Product Price is required'],
-    },
-    description: {
-        type: String,
-        required: [true, 'Please Provide Description Of Product!'],
-    },
-    image:{
-        type:String,
-        required:[true,'please provide image of product']
-    },
-    category: {
-        type: String,
-        required: [true, 'Please Select A Catgory Of Product'],
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 120,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // export model
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
