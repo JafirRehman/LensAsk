f1997d43c4ddff624792aebd847789c05d423ded
JafirRehman <mjafirrehman@gmail.com>
2024-12-29 23:04:53 +0500

    remove useless coments

--- Server/middlewares/Auth_middlewares.js
+++ Server/middlewares/Auth_middlewares.js
@@ -5,22 +5,17 @@ require("dotenv").config();
 // This middleware is used to check if user is authenticated or not
 exports.auth = async (req, res, next) => {
   try {
-    //get cookie
     const token = await req.cookies.token;
 
-    // validate token
     if (!token) {
       throw new Error("Login First!");
     }
     try {
-      //verify token
       const decode = jwt.verify(token, process.env.JWT_SECRET);
-      //store token to user
       req.user = decode;
     } catch (error) {
       throw new Error("Invalid Token!");
     }
-    // move to next work
     next();
   } catch (error) {
     return res.status(401).json({
@@ -33,13 +28,10 @@ exports.auth = async (req, res, next) => {
 // This middleware is used to check if user is customer or not
 exports.isCustomer = async (req, res, next) => {
   try {
-    //find user
     const userDetails = await User.findOne({ email: req.user.email });
-    //validate user
     if (userDetails.role !== "Customer") {
       throw new Error("Protected Route for customers");
     }
-    //move to next work
     next();
   } catch (error) {
     return res.status(500).json({
@@ -48,15 +40,13 @@ exports.isCustomer = async (req, res, next) => {
     });
   }
 };
+// This middleware is used to check if user is admin or not
 exports.isAdmin = async (req, res, next) => {
   try {
-    //find user
     const userDetails = await User.findOne({ email: req.user.email });
-    //validate user
     if (userDetails.role !== "Admin") {
       throw new Error("Protected Route for Admin");
     }
-    //move to next work
     next();
   } catch (error) {
     return res.status(500).json({
